941b8a01f5a6e5cecb90d0a0e6412bc8c3685c9b
Menna Saber <51786367+mennasaber@users.noreply.github.com>
2019-07-26 14:40:45 +0200

    Add menu to project

--- app/src/main/java/com/example/android/todolist/DB_helper.java
+++ app/src/main/java/com/example/android/todolist/DB_helper.java
@@ -29,11 +29,11 @@ public class DB_helper extends SQLiteOpenHelper {
                 + " INTEGER PRIMARY KEY AUTOINCREMENT ," + _Maintask_name
                 + " TEXT ," + _Maintask_checked + " BOOLIEAN" + ")";
 
-        String query2 =  "CREATE TABLE "
+        String query2 = "CREATE TABLE "
                 + TABlE_TASK + "(" + _Task_id
                 + "INTEGER PRIMARY KEY AUTOINCREMENT ,"
                 + _Name + "TEXT ," + _Checked + "BOOLEAN ,"
-                + _Priority + "INTEGER ," + _Time + "TEXT" +_Maintask_id +"INTEGER "+")";
+                + _Priority + "INTEGER ," + _Time + "TEXT" + _Maintask_id + "INTEGER " + ")";
         db.execSQL(query);
         db.execSQL(query2);
     }
@@ -45,34 +45,37 @@ public class DB_helper extends SQLiteOpenHelper {
 
     public void add_Item(item Item) {
         ContentValues values = new ContentValues();
-        values.put(_Maintask_name ,Item.getIname());
-        values.put(_Maintask_checked ,Item.isiChecked());
-        SQLiteDatabase db=getWritableDatabase();
-        db.insert(TABLE_ITEM ,null,values);
+        values.put(_Maintask_name, Item.getIname());
+        values.put(_Maintask_checked, Item.isiChecked());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_ITEM, null, values);
         db.close();
     }
+
     public void add_task(task Task) {
         ContentValues values = new ContentValues();
-        values.put( _Name,Task.getIname());
-        values.put(_Checked ,Task.isiChecked());
-        values.put(_Priority,Task.getiPriority());
-        values.put(_Time ,Task.getiTime());
-        values.put(_Checked ,Task.isiChecked());
-        values.put( _Maintask_id,Task.getiID());
-        SQLiteDatabase db=getWritableDatabase();
-        db.insert(TABLE_ITEM ,null,values);
+        values.put(_Name, Task.getIname());
+        values.put(_Checked, Task.isiChecked());
+        values.put(_Priority, Task.getiPriority());
+        values.put(_Time, Task.getiTime());
+        values.put(_Checked, Task.isiChecked());
+        values.put(_Maintask_id, Task.getiID());
+        SQLiteDatabase db = getWritableDatabase();
+        db.insert(TABLE_ITEM, null, values);
         db.close();
     }
-    public void appdate_item(item Item){
-       ContentValues values=new ContentValues();
-       values.put(_Maintask_checked,Item.isiChecked());
-        SQLiteDatabase db=getWritableDatabase();
-        db.update(TABLE_ITEM,values,"_Maintask_id="+Item.getiID(),null);
+
+    public void update_item(item Item) {
+        ContentValues values = new ContentValues();
+        values.put(_Maintask_checked, Item.isiChecked());
+        SQLiteDatabase db = getWritableDatabase();
+        db.update(TABLE_ITEM, values, "_Maintask_id=" + Item.getiID(), null);
     }
-    public void appdate_task(task Task){
-        ContentValues values=new ContentValues();
-        values.put(_Checked,Task.isiChecked());
-        SQLiteDatabase db=getWritableDatabase();
-        db.update(TABlE_TASK,values,"_task_id="+Task.getiID(),null);
+
+    public void update_task(task Task) {
+        ContentValues values = new ContentValues();
+        values.put(_Checked, Task.isiChecked());
+        SQLiteDatabase db = getWritableDatabase();
+        db.update(TABlE_TASK, values, "_task_id=" + Task.getiID(), null);
     }
 }
