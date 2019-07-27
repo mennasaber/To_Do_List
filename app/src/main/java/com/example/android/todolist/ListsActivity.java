94125e9b0c2ebdd6157b0dcb166f8545956eb171
mayarkhaled <miarkhaled3@gmail.com>
2019-07-27 02:45:42 +0200

    .

--- app/src/main/java/com/example/android/todolist/ListsActivity.java
+++ app/src/main/java/com/example/android/todolist/ListsActivity.java
@@ -1,12 +1,44 @@
 package com.example.android.todolist;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListsActivity extends AppCompatActivity {
 
+    EditText listTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
+        listTitle = (EditText) findViewById(R.id.listTitle);
+        listTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO --> if title is null add new title to db , if not update it in db
+            }
+        });
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.add:
+                //TODO --> pop up dialog has edit text and button , add task , put it in list
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.todolist;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.add:
+                //TODO --> intent to lists activity that will be empty
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
