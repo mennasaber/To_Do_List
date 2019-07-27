e8297db97e611727fb91f1d53dff7c4a052922fe
mayarkhaled <miarkhaled3@gmail.com>
2019-07-27 15:32:20 +0200

    add new xml file

--- app/src/main/java/com/example/android/todolist/ListsActivity.java
+++ app/src/main/java/com/example/android/todolist/ListsActivity.java
@@ -1,21 +1,30 @@
 package com.example.android.todolist;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ListsActivity extends AppCompatActivity {
 
     EditText listTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lists);
+
         listTitle = (EditText) findViewById(R.id.listTitle);
         listTitle.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +33,23 @@ public class ListsActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private void PopDialog()
+    {
+
+        final AlertDialog dialogBuilder = new AlertDialog.Builder(this).create();
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.add_task, null);
+       final EditText addTask = dialogView.findViewById(R.id.addNewTask);
+       Button add = dialogView.findViewById(R.id.buttonAdd);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO --> take from "add task" edit text
+            }
+        });
+        dialogBuilder.setView(dialogView);
+        dialogBuilder.show();
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,6 +63,7 @@ public class ListsActivity extends AppCompatActivity {
         {
             case R.id.add:
                 //TODO --> pop up dialog has edit text and button , add task , put it in list
+                PopDialog();
                 return true;
         }
         return super.onOptionsItemSelected(item);
